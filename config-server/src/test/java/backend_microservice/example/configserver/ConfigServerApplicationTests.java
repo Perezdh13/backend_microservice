9e46481d6aba48ec2d06bd6ab910b86116d88e98
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-10-02 18:58:46 +0200

    second round server microservices

---
+++ config-server/src/test/java/backend_microservice/example/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_microservice.example.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
