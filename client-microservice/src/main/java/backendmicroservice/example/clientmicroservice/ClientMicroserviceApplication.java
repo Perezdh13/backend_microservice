0c0cf307120625d56e9538cf29871cd425a07e7c
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-10-02 18:42:00 +0200

    create service configuration

---
+++ client-microservice/src/main/java/backendmicroservice/example/clientmicroservice/ClientMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package backendmicroservice.example.clientmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ClientMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientMicroserviceApplication.class, args);
+	}
+
+}
---
+++ employes-microservice/src/test/java/backendmicroservice/example/employesmicroservice/EmployesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package backendmicroservice.example.employesmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
